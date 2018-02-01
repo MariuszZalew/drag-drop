156cfefd8f91254955fd5e4159a7da3c38807e42
MariZal <m.zalew.poczta@gmail.com>
2018-02-01 15:38:51 +0100

    drag & drop project

---
+++ script/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $(".form-style label").draggable({ helper: "clone"});
+
+    $("#blue").children().draggable({ helper: "clone" });
+    var y =  $("#blue").children();
+    // console.log(y);
+    // $(".form-style").droppable({
+    //     drop: function (event, ui) {
+    //         // let element = $(ui.draggable).clone();
+    //         $(".drop form").append($(ui.draggable));
+    //     }
+    // });
+
+    // $("#div1 span").draggable ({
+    //     helper : "clone",
+    //     stop : function (event, ui)
+    //     {
+    //       ui.helper.clone ().appendTo ($(this).parent ());
+    //       $(this).remove ();
+    //     }
+    //   });
+
+
+    // $("#div1 span").draggable ({
+    //     helper : "clone",
+    //     stop : function (event, ui)
+    //     {
+    //       ui.helper.clone ().appendTo ($(this).parent ());
+    //     }
+    //   });
+
+    $(".drop").droppable({
+        drop: function (event, ui) {
+            let element = $(ui.draggable).clone();
+            $(".drop form").append(element);
+        }
+    });
+
+    
+
+    $("#button").click(function (e) {
+        var jsonData = {};
+
+        var formData = $("#blue").serializeArray();
+
+        $.each(formData, function () {
+            if (jsonData[this.name]) {
+                if (!jsonData[this.name].push) {
+                    jsonData[this.name] = [jsonData[this.name]];
+                }
+                jsonData[this.name].push(this.value || '');
+            } else {
+                jsonData[this.name] = this.value || '';
+            }
+        });
+        let per = JSON.stringify(jsonData);
+        // console.log(per);
+        console.log(y);
+        $(".resolution").empty();
+        $(".resolution").append(per);
+    });
+});
